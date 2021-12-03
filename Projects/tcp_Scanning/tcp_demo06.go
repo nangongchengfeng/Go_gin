5f10fe5962add352e711c79a6a88ad02234d9477
魏栋梁 <dongliang.wei@nnct-nsn.com>
2021-12-03 18:12:46 +0800

    tcp_demo06.go channel 并发控制

---
+++ Projects/tcp_Scanning/tcp_demo06.go
@@ -0,0 +1,50 @@
+package main
+ 
+import (
+	"code/Projects/tcp_Scanning/golimit"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+ 
+var wg sync.WaitGroup
+var mutex sync.Mutex
+ 
+func isOpen(host string, port int, timeout time.Duration) bool {
+ 
+	time.Sleep(time.Millisecond * 1)
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", host, port), timeout)
+	if err == nil {
+		_ = conn.Close()
+		return true
+	}
+ 
+	return false
+}
+ 
+func main() {
+	startTime := time.Now()
+	ports := []int{}
+	timeout := time.Millisecond * 500
+	g := golimit.NewGoLimit(2000)
+	for port := 1; port <= 65000; port++ {
+		g.Add()
+		go func(g *golimit.GoLimit, p int) {
+			opened := isOpen("10.10.10.1", p, timeout)
+			if opened {
+				mutex.Lock()
+				ports = append(ports, p)
+				log.Printf("端口: %d 已经开通", p)
+				mutex.Unlock()
+			}
+			g.Done()
+		}(g, port)
+	}
+	time.Since(startTime)
+	cost := int(time.Since(startTime) / time.Second)
+	fmt.Printf("opened ports: %v\n", ports)
+ 
+	fmt.Printf("代码运行时长: %d S", cost)
+}
